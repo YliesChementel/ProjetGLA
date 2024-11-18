25e78a4c5377b59db17039439b79bf48e91d21c2
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2024-11-18 10:23:31 +0100

    Modification du fichier Api.java pour qu'il tourne en permanence

--- Api.java
+++ Api.java
@@ -14,12 +14,16 @@ public class Api {
                     .header("Accept", "application/json")
                     .build();
 
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            while (true) {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            System.out.println("Statut de la réponse: " + response.statusCode());
-            System.out.println("Corps de la réponse: " + response.body());
-            HttpHeaders headers = response.headers();
-            headers.map().forEach((k,v) -> System.out.println(k + ":" + v));
+                System.out.println("Statut de la réponse: " + response.statusCode());
+                System.out.println("Corps de la réponse: " + response.body());
+                HttpHeaders headers = response.headers();
+                headers.map().forEach((k,v) -> System.out.println(k + ":" + v));
+
+                Thread.sleep(30000);  // 20000 millisecondes = 20 secondes
+            }
             
         } catch (Exception e) {
             e.printStackTrace();
