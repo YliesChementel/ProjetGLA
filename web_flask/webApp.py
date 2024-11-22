bda80105f4d00102ac1b75d959e6df0151955601
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2024-11-22 19:59:03 +0100

    Modification du site pour inclure les données de la DB

--- web_flask/webApp.py
+++ web_flask/webApp.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
-
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    connCrypto = sqlite3.connect('Crypto.db')  
+    connCryptoData = sqlite3.connect('CryptoData.db')  
+    
+    connCrypto.row_factory = sqlite3.Row
+    connCryptoData.row_factory = sqlite3.Row
+    
+    crypto_data = connCrypto.execute('SELECT * FROM Crypto').fetchall()
+    cryptoData_data = connCryptoData.execute('SELECT * FROM CryptoData').fetchall()
+ 
+    connCrypto.close()
+    connCryptoData.close()
+    
+    return render_template('index.html', crypto_data=crypto_data,cryptoData_data=cryptoData_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
