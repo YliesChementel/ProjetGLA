95b5dc8c295b92f4777e52e405cad02ce2dd96ec
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2024-11-23 19:37:11 +0100

    Modification du site pour inclure les graphes des volumes

--- web_flask/webApp.py
+++ web_flask/webApp.py
@@ -15,93 +15,98 @@ def index():
  
     connCrypto.close()
     
-    prices = graphPrices(cryptoData_data)
+    prices, volumes = graphPricesAndVolumes(cryptoData_data)
 
     graphPriceBTC = prices[0].to_html(full_html=False)
     graphPriceETH = prices[1].to_html(full_html=False)
     graphPriceUDST = prices[2].to_html(full_html=False)
     graphPriceSOL = prices[3].to_html(full_html=False)
     graphPriceBNB = prices[4].to_html(full_html=False)
-    
-    return render_template('index.html', crypto_data=crypto_data,cryptoData_data=cryptoData_data,
-    graphPriceBTC=graphPriceBTC,
-    graphPriceETH=graphPriceETH,
-    graphPriceUDST=graphPriceUDST,
-    graphPriceSOL=graphPriceSOL,
-    graphPriceBNB=graphPriceBNB)
 
-def graphPrices(cryptoData_data):
-    datesBTC = []
-    datesETH = []
-    datesUSDT = []
-    datesSOL = []
-    datesBNB = []
-    pricesBTC = []
-    pricesETH = []
-    pricesUSDT = []
-    pricesSOL= []
-    pricesBNB = []
+    graphVolumeBTC = volumes[0].to_html(full_html=False)
+    graphVolumeETH = volumes[1].to_html(full_html=False)
+    graphVolumeUDST = volumes[2].to_html(full_html=False)
+    graphVolumeSOL = volumes[3].to_html(full_html=False)
+    graphVolumeBNB = volumes[4].to_html(full_html=False)
     
-    for data in cryptoData_data:##C'est la même date pour toutes les donnnées donc peut-être changer ça
-        if(data['crypto_id']=="bitcoin"):
-            datesBTC.append(data['fetchTime'])
+    return render_template(
+        'index.html',
+        crypto_data=crypto_data,
+        cryptoData_data=cryptoData_data,
+        graphPriceBTC=graphPriceBTC,
+        graphPriceETH=graphPriceETH,
+        graphPriceUDST=graphPriceUDST,
+        graphPriceSOL=graphPriceSOL,
+        graphPriceBNB=graphPriceBNB,
+        graphVolumeBTC=graphVolumeBTC,
+        graphVolumeETH=graphVolumeETH,
+        graphVolumeUDST=graphVolumeUDST,
+        graphVolumeSOL=graphVolumeSOL,
+        graphVolumeBNB=graphVolumeBNB
+    )
+
+def graphPricesAndVolumes(cryptoData_data):
+    dates = []
+    pricesBTC, pricesETH, pricesUSDT, pricesSOL, pricesBNB = [], [], [], [], []
+    volumesBTC, volumesETH, volumesUSDT, volumesSOL, volumesBNB = [], [], [], [], []
+
+    for data in cryptoData_data:
+        if data['crypto_id'] == "bitcoin":
             pricesBTC.append(data['price'])
-        if(data['crypto_id']=="ethereum"):
-            datesETH.append(data['fetchTime'])
+            volumesBTC.append(data['volume'])
+            dates.append(data['fetchTime'])
+        if data['crypto_id'] == "ethereum":
             pricesETH.append(data['price'])
-        if(data['crypto_id']=="tether"):
-            datesUSDT.append(data['fetchTime'])
+            volumesETH.append(data['volume'])
+        if data['crypto_id'] == "tether":
             pricesUSDT.append(data['price'])
-        if(data['crypto_id']=="solana"):
-            datesSOL.append(data['fetchTime'])
+            volumesUSDT.append(data['volume'])
+        if data['crypto_id'] == "solana":
             pricesSOL.append(data['price'])
-        if(data['crypto_id']=="binance-coin"):
-            datesBNB.append(data['fetchTime'])
+            volumesSOL.append(data['volume'])
+        if data['crypto_id'] == "binance-coin":
             pricesBNB.append(data['price'])
-
-    # Création du graphique
+            volumesBNB.append(data['volume'])
+    
+    # Création des graphiques pour les prix
     figBTC = go.Figure()
     figETH = go.Figure()
     figUDST = go.Figure()
     figSOL = go.Figure()
     figBNB = go.Figure()
 
-    # Ajout d'une trace
-    figBTC.add_trace(go.Scatter(x=datesBTC, y=pricesBTC, mode='lines+markers', name='Prix Bitcoin'))
-    figETH.add_trace(go.Scatter(x=datesETH, y=pricesETH, mode='lines+markers', name='Prix Ethereum'))
-    figUDST.add_trace(go.Scatter(x=datesUSDT, y=pricesUSDT, mode='lines+markers', name='Prix Tether'))
-    figSOL.add_trace(go.Scatter(x=datesSOL, y=pricesSOL, mode='lines+markers', name='Prix Solana'))
-    figBNB.add_trace(go.Scatter(x=datesBNB, y=pricesBNB, mode='lines+markers', name='Prix BNB'))
+    figBTC.add_trace(go.Scatter(x=dates, y=pricesBTC, name='Prix Bitcoin'))
+    figETH.add_trace(go.Scatter(x=dates, y=pricesETH,  name='Prix Ethereum'))
+    figUDST.add_trace(go.Scatter(x=dates, y=pricesUSDT, name='Prix Tether'))
+    figSOL.add_trace(go.Scatter(x=dates, y=pricesSOL, name='Prix Solana'))
+    figBNB.add_trace(go.Scatter(x=dates, y=pricesBNB, name='Prix BNB'))
 
-    # Titres et labels
-    figBTC.update_layout(
-        title='Évolution du Prix du BTC',
-        xaxis_title='Date',
-        yaxis_title='Prix (USD)',
-    )
-    figETH.update_layout(
-        title='Évolution du Prix du ETH',
-        xaxis_title='Date',
-        yaxis_title='Prix (USD)',
-    )
-    figUDST.update_layout(
-        title='Évolution du Prix du UDST',
-        xaxis_title='Date',
-        yaxis_title='Prix (USD)',
-    )
-    figSOL.update_layout(
-        title='Évolution du Prix du SOL',
-        xaxis_title='Date',
-        yaxis_title='Prix (USD)',
-    )
-    figBNB.update_layout(
-        title='Évolution du Prix du BNB',
-        xaxis_title='Date',
-        yaxis_title='Prix (USD)',
-    )
+    figBTC.update_layout(title='Évolution du Prix du BTC', xaxis_title='Date', yaxis_title='Prix (USD)')
+    figETH.update_layout(title='Évolution du Prix du ETH', xaxis_title='Date', yaxis_title='Prix (USD)')
+    figUDST.update_layout(title='Évolution du Prix du UDST', xaxis_title='Date', yaxis_title='Prix (USD)')
+    figSOL.update_layout(title='Évolution du Prix du SOL', xaxis_title='Date', yaxis_title='Prix (USD)')
+    figBNB.update_layout(title='Évolution du Prix du BNB', xaxis_title='Date', yaxis_title='Prix (USD)')
 
-    return figBTC,figETH,figUDST,figSOL,figBNB
-   
+    # Création des graphiques pour les volumes
+    volBTC = go.Figure()
+    volETH = go.Figure()
+    volUDST = go.Figure()
+    volSOL = go.Figure()
+    volBNB = go.Figure()
+
+    volBTC.add_trace(go.Scatter(x=dates, y=volumesBTC, name='Volume Bitcoin'))
+    volETH.add_trace(go.Scatter(x=dates, y=volumesETH, name='Volume Ethereum'))
+    volUDST.add_trace(go.Scatter(x=dates, y=volumesUSDT, name='Volume Tether'))
+    volSOL.add_trace(go.Scatter(x=dates, y=volumesSOL, name='Volume Solana'))
+    volBNB.add_trace(go.Scatter(x=dates, y=volumesBNB, name='Volume BNB'))
+
+    volBTC.update_layout(title='Évolution du Volume du BTC', xaxis_title='Date', yaxis_title='Volume')
+    volETH.update_layout(title='Évolution du Volume du ETH', xaxis_title='Date', yaxis_title='Volume')
+    volUDST.update_layout(title='Évolution du Volume du UDST', xaxis_title='Date', yaxis_title='Volume')
+    volSOL.update_layout(title='Évolution du Volume du SOL', xaxis_title='Date', yaxis_title='Volume')
+    volBNB.update_layout(title='Évolution du Volume du BNB', xaxis_title='Date', yaxis_title='Volume')
+
+    return (figBTC, figETH, figUDST, figSOL, figBNB), (volBTC, volETH, volUDST, volSOL, volBNB)
 
 if __name__ == '__main__':
     app.run(debug=True)
