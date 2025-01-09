9ccdde5c49cc9072b5bbdd3f8e7e6d2c27672035
YliesChementel <145006556+YliesChementel@users.noreply.github.com>
2025-01-09 13:29:35 +0100

    Réorganisation (ça a pas marcher la dernière fois)

---
+++ AppConsole/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import static org.example.CryptoDataBase.*;
+
+public class
+Main {
+    public static void main(String[] args) throws SQLException, IOException, InterruptedException {
+        Logger logger = Logger.getLogger(Api.class.getName());
+        Api api = new Api(logger);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = api.takeApiRequest("https://api.coincap.io/v2/assets");
+
+        //Vérification de si les tables existe
+        String cryptoDB = "jdbc:sqlite:../instance/Crypto.db";
+        try (Connection conn = DriverManager.getConnection(cryptoDB)) {
+            if (!tableExists(conn, "Crypto")) {
+                createCrypto(conn);
+            }
+            if (!tableExists(conn, "CryptoData")) {
+                createCryptoData(conn);
+            }
+            api.apiRun(conn, client, request,10);
+        }
+    }
+}
--- tests/config_test.py
+++
@@ -1,5 +0,0 @@
-class TestConfig:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    TESTING = True
-    SECRET_KEY = 'test_secret'
-    WTF_CSRF_ENABLED = False
